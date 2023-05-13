f27848ba0b7daab585221e6141099f1b04e744d7
Matthew Wolfe <matthew.wolfe2@gmail.com>
2023-05-13 16:01:57 -0400

    implement groupBy

---
+++ src/utils/groupBy/groupBy.spec.ts
@@ -0,0 +1,43 @@
+import { groupBy } from './groupBy';
+
+interface User {
+  id: string;
+  firstName: string;
+  lastName: string;
+  state: string;
+}
+
+const users: User[] = [
+  {
+    id: '1',
+    firstName: 'Test1',
+    lastName: 'User1',
+    state: 'VA',
+  },
+  {
+    id: '2',
+    firstName: 'Test2',
+    lastName: 'User2',
+    state: 'CO',
+  },
+  {
+    id: '3',
+    firstName: 'Test3',
+    lastName: 'User3',
+    state: 'VA',
+  },
+];
+
+describe('groupBy', () => {
+  test('state', () => {
+    const result = groupBy(users, 'state');
+
+    expect(Object.fromEntries(result)).toEqual({
+      VA: [
+        { id: '1', firstName: 'Test1', lastName: 'User1', state: 'VA' },
+        { id: '3', firstName: 'Test3', lastName: 'User3', state: 'VA' },
+      ],
+      CO: [{ id: '2', firstName: 'Test2', lastName: 'User2', state: 'CO' }],
+    });
+  });
+});
--- src/utils/groupBy/groupBy.ts
+++ src/utils/groupBy/groupBy.ts
@@ -1,1 +1,14 @@
-export function groupBy() {}
+import type { TypeofArray } from 'types/array';
+
+export function groupBy<T extends TypeofArray<T>[], P extends keyof TypeofArray<T>>(
+  array: T,
+  property: P
+) {
+  const map = new Map<TypeofArray<T>[P], TypeofArray<T>[]>();
+
+  array.forEach((element: TypeofArray<T>) => {
+    map.set(element[property], [...(map.get(element[property]) || []), element]);
+  });
+
+  return map;
+}
