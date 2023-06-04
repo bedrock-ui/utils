59298543c6ae1cdae942ec5d66ba453b7b814dfb
Matthew Wolfe <matthew.wolfe2@gmail.com>
2023-06-03 21:45:45 -0400

    countBy override (#1)
    * countBy override to work with array or function

--- src/utils/countBy/countBy.spec.ts
+++ src/utils/countBy/countBy.spec.ts
@@ -1,7 +1,7 @@
 import { countBy } from './countBy';
 
 interface User {
-  id: string;
+  id: number;
   firstName: string;
   lastName: string;
   state: string;
@@ -9,19 +9,19 @@ interface User {
 
 const users: User[] = [
   {
-    id: '1',
+    id: 1,
     firstName: 'Test1',
     lastName: 'User1',
     state: 'VA',
   },
   {
-    id: '2',
+    id: 2,
     firstName: 'Test2',
     lastName: 'User2',
     state: 'CO',
   },
   {
-    id: '3',
+    id: 3,
     firstName: 'Test3',
     lastName: 'User3',
     state: 'VA',
@@ -31,6 +31,15 @@ const users: User[] = [
 describe('countBy', () => {
   test('state', () => {
     const result = countBy(users, 'state');
+
+    expect(Object.fromEntries(result)).toEqual({
+      VA: 2,
+      CO: 1,
+    });
+  });
+
+  test('state with function', () => {
+    const result = countBy(users, (user) => user.state);
 
     expect(Object.fromEntries(result)).toEqual({
       VA: 2,
--- src/utils/countBy/countBy.ts
+++ src/utils/countBy/countBy.ts
@@ -1,14 +1,41 @@
 import type { TypeofArray } from 'types/array';
 
-export function countBy<T extends TypeofArray<T>[], P extends keyof TypeofArray<T>>(
+type FnParam<T extends TypeofArray<T>[]> = (element: TypeofArray<T>) => unknown;
+
+type ArrayParam<T> = keyof TypeofArray<T>;
+
+type CombinedReturn<
+  T extends TypeofArray<T>[],
+  P extends ArrayParam<T> | FnParam<T>
+> = P extends ArrayParam<T> ? TypeofArray<T>[P] : P extends FnParam<T> ? ReturnType<P> : never;
+
+function countBy<T extends TypeofArray<T>[], P extends FnParam<T>>(
   array: T,
   property: P
-) {
-  const map = new Map<TypeofArray<T>[P], number>();
+): Map<ReturnType<P>, number>;
+
+function countBy<T extends TypeofArray<T>[], P extends ArrayParam<T>>(
+  array: T,
+  property: P
+): Map<TypeofArray<T>[P], number>;
+
+function countBy<T extends TypeofArray<T>[], P extends ArrayParam<T> | FnParam<T>>(
+  array: T,
+  property: P
+): Map<CombinedReturn<T, P>, number> {
+  const map = new Map();
 
   array.forEach((element: TypeofArray<T>) => {
-    map.set(element[property], (map.get(element[property]) || 0) + 1);
+    if (typeof property === 'function') {
+      const value = property(element);
+      map.set(value, (map.get(value) || 0) + 1);
+    } else {
+      const value = property as keyof TypeofArray<T>;
+      map.set(element[value], (map.get(element[value]) || 0) + 1);
+    }
   });
 
   return map;
 }
+
+export { countBy };
